3355112ff52287cd6a769197543bc7301a44ad6a
cperezll <cperez2826@gmail.com>
2018-11-14 16:53:58 +0100

    comunication with baseballcoach

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -3,8 +3,12 @@ package com.luv2code.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//create the object
+		BaseBallCoach theCoach = new BaseBallCoach();
+		
+		//use the object
+		System.out.println(theCoach.getDeilyWorkout());
 	}
 
 }
